b03608c79ac975d161587831ffd9c9eb2b9b0208
Wahid Maruf <wahid.maruf@gmail.com>
2024-05-22 23:47:20 -0700

    Update script

--- fedora/install4.sh
+++ fedora/install4.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-cd ~
-mkdir -p dev
-cd ~/dev
-wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.22.1-stable.tar.xz
-tar xf flutter_linux_*-stable.tar.xz
-rm flutter_linux_*-stable.tar.xz
+mkdir -p ~/dev
+wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.22.1-stable.tar.xz -P ~/dev
+tar xf ~/dev/flutter_linux_*-stable.tar.xz -C ~/dev && rm ~/dev/flutter_linux_*-stable.tar.xz
 
 echo 'export PATH="$PATH:~/dev/flutter/bin"' >> ~/.bashrc
 echo 'export CHROME_EXECUTABLE=/opt/google/chrome/google-chrome' >> ~/.bashrc
